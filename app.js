f677362eefc7ecd2da4d78dfaf9473739c0f773c
Baptiste <Baptiste.grias@gmail.com>
2022-05-19 15:59:11 -0700

    ball slider and bricks created

--- app.js
+++ app.js
@@ -29,3 +29,36 @@ drawSlider();
 
 // array with the bricks
 
+const bricks = [];
+
+for(let i = 0; i < nbRow; i++) {
+  bricks[i] = [];
+  for(let j = 0; j < nbCol; j++) {
+    bricks[i][j] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+// console.log(bricks);
+
+function drawBricks() {
+    for(let i = 0; i < nbRow; i++) {
+        for(let j = 0; j < nbCol; j++) {
+        
+            if(bricks[i][j].status == 1) {
+
+                let brickX = (j * (brickWidth + 10) + 35) 
+                let brickY = (i * (brickHeight + 10) + 30)
+                
+                bricks[i][j].x = brickX;
+                bricks[i][j].y = brickY;
+
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#333';
+                ctx.fill();
+                ctx.closePath();
+        }
+        }
+    }
+}
+drawBricks();
